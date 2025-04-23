bddba367324da22944ea3618754313ccd5b0b04e
Susilvav03 <144504485+Susilvav03@users.noreply.github.com>
2025-04-23 18:07:45 -0500

    SOYUNADOBLEBOBA

--- Semana1/Ej10_rango.py
+++ Semana1/Ej10_rango.py
@@ -7,6 +7,6 @@ Pide un número al usuario. Di si está dentro del rango de 1 a 10 (inclusive).
 num = int(input("Hola, ingresa un número: "))
 
 if 1<= num <= 10:
-    print("El número está dentro del rango")
+    print("El número está dentro del rango de 1 a 10")
 else:
     print("El número no está dentro del rango")
