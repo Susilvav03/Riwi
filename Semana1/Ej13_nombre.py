6e64b8cc97b6bf80f5dbe6c87b173404af768ba9
Susilvav03 <144504485+Susilvav03@users.noreply.github.com>
2025-04-30 18:38:34 -0500

    Create Ej13_nombre.py

---
+++ Semana1/Ej13_nombre.py
@@ -0,0 +1,15 @@
+"""
+3. Lista vacía y append()
+
+    Crea una lista vacía llamada nombres.
+    Pide al usuario su nombre y agrégalo a la lista usando append().
+    Muestra la lista final.
+
+"""
+
+Names = []
+
+UserName = input("¡Hola! por favor ingresa tu nombre: ")
+
+Names.append(UserName)
+print(Names)
